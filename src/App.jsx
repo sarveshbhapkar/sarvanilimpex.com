24d86b37ecfcf2d4df875fda33483ea239d8f0ab
sarvesh bhapkar <sarveshbhapkar100@gmail.com>
2026-02-27 19:55:55 +0530

    Update routing and navbar styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -24,6 +24,7 @@ function App() {
             <Route path="/global" element={<GlobalPresence />} />
             <Route path="/certifications" element={<Certifications />} />
             <Route path="/contact" element={<Contact />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
           </Routes>
         </main>
         <Footer />
